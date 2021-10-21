94363b190c0f93727dd6b1e3d5b8f2cb97efc3a4
Aged-cat <2324261907@qq.com>
2021-10-21 17:31:00 +0800

    codetop

--- codetop/offer10.cpp
+++ codetop/offer10.cpp
@@ -3,14 +3,29 @@
 using namespace std;
 
 int numWays(int n){
-    vector<int> dp(n + 1, 0);
+    vector<long long> dp(n + 1, 0);
     dp[0] = 1;
 
     for(int i = 1; i <= n; ++i){
-        dp[i] = (i >= 1 ? dp[i - 1] : 0) + (i >= 2 ? dp[i - 2] : 0);
+        dp[i] = dp[i - 1] + (i >= 2 ? dp[i - 2] : 0);
     }
 
     return dp[n] % 1000000007;
+}
+
+int numWays(int n){
+    int a = 1;
+    int b = 1;
+
+    if(n == 0 || n == 1) return 1;
+
+    for(int i = 2; i <= n; ++i){
+        int t = (a % 1000000007 + b % 1000000007) % 1000000007;
+        b = a;
+        a = t;
+    }
+
+    return a;
 }
 
 int main(void)
